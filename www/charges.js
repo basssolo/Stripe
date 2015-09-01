ba361d4c68d3f042bd209249d124bec5fb23c9f6
Pasi Häkkinen <basssolo@users.noreply.github.com>
2015-09-01 18:35:10 +0300

    Update charges.js
    Added Charges.capture()

--- www/charges.js
+++ www/charges.js
@@ -19,6 +19,10 @@ Charges.prototype.update = function(id, arg, successCallback, errorCallback){
     this.processRequest(["POST","charges/" + id, arg], successCallback, errorCallback);
 }
 
+Charges.prototype.capture = function(id, arg, successCallback, errorCallback){
+    this.processRequest(["POST","charges/" + id, arg], successCallback, errorCallback);
+}
+
 Charges.prototype.list = function(successCallback, errorCallback){
   this.processRequest(["GET","charges", null], successCallback, errorCallback);
 }
