6a4377bddb6ec7246be34927f664920639f2f3be
Pasi Häkkinen <basssolo@users.noreply.github.com>
2015-09-03 10:18:23 +0300

    Update refunds.js

--- www/refunds.js
+++ www/refunds.js
@@ -1,13 +1,13 @@
 var exec = require('cordova/exec');
 var channel = require('cordova/channel');
 
-function Charges(){
+function Refunds(){
   this.processRequest = function(args, cb, error){
       exec(cb,error, "Stripe", "process", args);
   };
 }
 
-Charges.prototype.create = function(arg, successCallback, errorCallback){
+Refunds.prototype.create = function(arg, successCallback, errorCallback){
     this.processRequest(["POST","refunds", arg], successCallback, errorCallback);
 }
 
